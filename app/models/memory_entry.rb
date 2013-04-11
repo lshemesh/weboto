3f991ea4217c9cb86110e8b0cfb7611d5546b879
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 12:35:35 -0400

    dependent destroy for entries and definitions
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

--- app/models/memory_entry.rb
+++ app/models/memory_entry.rb
@@ -1,5 +1,5 @@
 class MemoryEntry < ActiveRecord::Base
-  has_many :memory_definitions
+  has_many :memory_definitions, :dependent => :destroy
 
   validates :name, presence: true, uniqueness: true
 end
