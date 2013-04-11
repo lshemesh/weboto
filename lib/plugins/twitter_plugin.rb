8eaf4b241166abc9cc1566f7bbfc54ebe5a2d316
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 19:29:20 -0400

    twitter plugins requests a handle
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

--- lib/plugins/twitter_plugin.rb
+++ lib/plugins/twitter_plugin.rb
@@ -4,10 +4,14 @@ class TwitterPlugin
   match /tweet ([^\s]+)/
 
   def execute(m, handle)
-    Rails.logger.info("Twitter plugin firing for handle #{handle}")
+    if handle
+      Rails.logger.info("Twitter plugin firing for handle #{handle}")
 
-    TwitterStatusFetcher.new(handle).statuses.each do |status|
-      m.reply status.formatted_status
+      TwitterStatusFetcher.new(handle).statuses.each do |status|
+        m.reply status.formatted_status
+      end
+    else
+      m.reply "Please provide a handle like: !tweets WeWork"
     end
   end
 end
