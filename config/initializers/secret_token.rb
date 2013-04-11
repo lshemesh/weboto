f9ea1c02d5f2f4a2c5f577e6b5a90899b6152784
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 09:52:33 -0400

    initial commit
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weboto::Application.config.secret_token = 'ece552ad2aca9bc6055f337b11c90a8426619d9e4cb19c5fc506b4f9f4b752c7b1a539c6de63e0de01333e2351b41aee5046e30796e038c8204ee56ce3c29354'
