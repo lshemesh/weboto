a760f9acc9fb3b3938c1ea9364da095521a48489
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 18:27:53 -0400

    fixed broken spec
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

--- spec/lib/plugins/twitter_plugin_spec.rb
+++ spec/lib/plugins/twitter_plugin_spec.rb
@@ -1,20 +1,7 @@
 require 'spec_helper'
 
 describe TwitterPlugin do
-
-  let(:client) {HTTParty}
-  let(:twitter_plugin) {TwitterPlugin}
-
   it 'is a cinch plugin' do
-    twitter_plugin.should include(Cinch::Plugin)
+    TwitterPlugin.should include(Cinch::Plugin)
   end
-
-  describe "#statuses" do
-    it "should return a list of statuses from WeWork account" do
-      expected = ["name" => "WeWork", "id" => 111092283]
-      @statuses = twitter_plugin.statuses('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=WeWork&count=2')
-      @statuses[0]["user"].should include *expected
-    end
-  end
-
 end
