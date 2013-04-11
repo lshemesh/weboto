9a73988aece2c6126dae9f2dab48920065150183
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 15:29:45 -0400

    don't break heroku with asset pipeline
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

--- config/application.rb
+++ config/application.rb
@@ -67,6 +67,9 @@ module Weboto
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    # don't break heroku
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.fixture_replacement :factory_girl
       g.test_framework(:rspec,
