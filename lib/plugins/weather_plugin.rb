b372b3310f17927ac02d50e4e92edecc219c44e9
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 17:53:52 -0400

    weather plugin in shape to test
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

--- lib/plugins/weather_plugin.rb
+++ lib/plugins/weather_plugin.rb
@@ -1,3 +1,19 @@
 class WeatherPlugin
   include Cinch::Plugin
+
+  match /weather/
+
+  def execute(m)
+    Rails.logger.info("Weather plugin firing")
+
+    WEWORK_LOCATIONS.each do |location, pair|
+      Rails.logger.info("Fetching weather for #{location}")
+
+      report = WeatherFether.new(location: location,
+                                 lat: pair['lat'],
+                                 lon: pair['lon']).weather_reports.first
+
+      m.reply report.formatted_report
+    end
+  end
 end
