e8176a9bb29b11f09b32e26bdee6d49d84f43c6b
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 17:54:22 -0400

    activated weather plugin
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

--- lib/irc_bot.rb
+++ lib/irc_bot.rb
@@ -10,7 +10,7 @@ class IrcBot
 
         c.channels        = ["#wework"]
         c.nick            = "weboto"
-        c.plugins.plugins = [MemoryPlugin, UptimePlugin, GithubCommitPlugin, UrlShortenerPlugin, HelpPlugin, StatsPlugin]
+        c.plugins.plugins = [MemoryPlugin, UptimePlugin, GithubCommitPlugin, UrlShortenerPlugin, HelpPlugin, StatsPlugin, WeatherPlugin]
         c.server          = "irc.freenode.org"
       end
     end
