0b90422193e2038f60cc3c7c95a96c8a9f309137
Jonathan E. Magen <jonathan@wework.com>
2013-04-11 21:30:25 -0400

    hello plugin
    Signed-off-by: Jonathan E. Magen <jonathan@wework.com>

--- lib/plugins/hello_plugin.rb
+++ lib/plugins/hello_plugin.rb
@@ -1,7 +1,7 @@
 class HelloPlugin
   include Cinch::Plugin
 
-  match /(hi|hello|hey|yo|ahoy)/, use_prefix: false
+  match /^(hi|hello|hey|yo|ahoy)$/, use_prefix: false
 
   def execute(m, greeting)
     m.reply("#{m.user.nick}: #{greeting}!")
